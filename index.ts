34f4941692a4ba61f0ff379cf4eb4b6f593fe111
s1avo <s.nadolny@hotmail.com>
2023-02-23 22:29:21 +0100

    Changed esModuleInterop on true and fixed imports.

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import * as express from "express";
-import * as dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import * as fileUpload from "express-fileupload";
-import * as cors from "cors";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import rateLimit from "express-rate-limit";
 import "express-async-errors";
 import { handleError } from "./utils/errors";
 import { viewRouter } from "./routes/view";
@@ -26,6 +27,13 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 
 //roots
 app.use("/", viewRouter);
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const authRouter = Router();
 
