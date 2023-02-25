548351fb98c456254bb2fa3d01aac24c9b69744c
s1avo <s.nadolny@hotmail.com>
2023-02-25 15:16:28 +0100

    Fixed issue with updating qty after product update.

--- records/product.records.ts
+++ records/product.records.ts
@@ -44,7 +44,7 @@ export class ProductRecords implements ProductEntity {
     if (!this.id) throw new ValidationErrors(`Wrong ID: ${this.id}`);
 
     await pool.execute(
-      "UPDATE `products` SET `name` = :name, `secondName` = :secondName, `qty` = :qty, `unit` = :unit, `place` = :place, `productDataSheet` = :productDataSheet, `active` = :active WHERE `id`= :id",
+      "UPDATE `products` SET `name` = :name, `secondName` = :secondName, `unit` = :unit, `place` = :place, `productDataSheet` = :productDataSheet, `active` = :active WHERE `id`= :id",
       this
     );
   }
