064d7f89b9dd6789587113ebe09e05fb7eb18c91
s1avo <s.nadolny@hotmail.com>
2023-07-27 16:45:00 +0200

    Issue on the server with access to db.

--- utils/db.ts
+++ utils/db.ts
@@ -1,10 +1,13 @@
 import { createPool } from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
   decimalNumbers: true,
   namedPlaceholders: true,
 });
